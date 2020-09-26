2366ae4406d8153c466ee0e0d4366681f3cb615c
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-09-26 13:10:45 -0300

    Instalação do pacote nuget e configuração do swagger na Startup

--- ApiSwagger/ApiSwagger/Startup.cs
+++ ApiSwagger/ApiSwagger/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace ApiSwagger
 {
@@ -24,6 +26,22 @@ namespace ApiSwagger
             .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+
+            // Swagger
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "To Do items",
+                        Version = "v1",
+                        Description = "Exemplo de API REST criada com o ASP.NET Core 3.1 para manipulação e consulta de ToDos",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Mateus Pereira",
+                            Url = new Uri("https://github.com/pereiracode")
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +57,13 @@ namespace ApiSwagger
             app.UseRouting();
 
             app.UseAuthorization();
+
+            //Swagger 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
