1347b896f7d3fa64084bde366184f103ee368b75
Mateus Pereira <mateus.p.dias@hotmail.com>
2020-09-26 00:13:44 -0300

    Controller ToDos e Primeira Migration

---
+++ ApiSwagger/ApiSwagger/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiSwagger.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiSwagger.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<ToDo> ToDos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+        }
+    }
+}
---
+++ ApiSwagger/ApiSwagger/Data/Mappings/ToDoMapping.cs
@@ -0,0 +1,19 @@
+﻿using ApiSwagger.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ApiSwagger.Data.Mappings
+{
+    public class ToDoMapping : IEntityTypeConfiguration<ToDo>
+    {
+        public void Configure(EntityTypeBuilder<ToDo> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Name).IsRequired().HasColumnType("varchar(50)");
+            builder.Property(t => t.IsComplete).IsRequired();
+
+            builder.ToTable("Todos");
+        }
+    }
+}
